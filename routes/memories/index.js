1361de41324dfc695261ff7bdf49f85810fd9656
Sky Winston <skywinston@gmail.com>
2015-11-12 15:39:12 -0700

    data is succesfully persisting in the post route locally

--- routes/memories/index.js
+++ routes/memories/index.js
@@ -5,11 +5,17 @@ var conString = process.env.POSTGRES_SERVER;
 
 router.route('/')
 .get(function(req, res){
-
+  pg.connect(conString, function(err, client, done){
+    client.query('SELECT * FROM memories', function(err, result){
+      done();
+      res.send(result);
+    });
+  });
 })
 .post(function(req, res){
+  var data = req.body.data.attributes;
   pg.connect(conString, function(err, client, done){
-    client.query('INSERT INTO memories (old_days, these_days, year) values ($1, $2, $3)', [req.body.old_days, req.body.these_days, req.body.year], function(err, result){
+    client.query('INSERT INTO memories (old_days, these_days, year) values ($1, $2, $3)', [data.old_days, data.these_days, data.year], function(err, result){
       done();
       if(err){
         res.send("An error occurred: ", err)
