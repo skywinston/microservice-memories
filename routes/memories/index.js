645b0014c375e90984ee3c2d4ec63008a8575e83
Sky Winston <skywinston@gmail.com>
2015-11-13 10:46:23 -0700

    Did error handling and response in the post to memories and now the API completely matches the spec

--- routes/memories/index.js
+++ routes/memories/index.js
@@ -17,14 +17,27 @@ router.route('/')
 })
 .post(function(req, res){
   var data = req.body.data.attributes;
+  var err = memoryFormatter.validate(req.body);
+  if(err.error.length > 0){
+    res.status(400).json(err);
+  }
   pg.connect(conString, function(err, client, done){
-    client.query('INSERT INTO memories (old_days, these_days, year) values ($1, $2, $3)', [data.old_days, data.these_days, Number(data.year)], function(err, result){
+    client.query('INSERT INTO memories (old_days, these_days, year) VALUES ($1, $2, $3)', [data.old_days, data.these_days, data.year], function(err, result){
       done();
       if(err){
-        res.send("An error occurred: ", err)
+        res.status(500).send(err);
       }
       // build an object per the spec to send back via res.json
-      var response = memoryFormatter.complete(result);
+      var response = {
+        "data": {
+          "type": "memory",
+          "attributes": {
+            "old_days": data.old_days,
+            "these_days": data.these_days,
+            "year": Number(data.year)
+          }
+        }
+      }
       res.json(response);
     });
   });
